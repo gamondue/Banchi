7327703939db189753af23782cc297754094cf38
Andrea Barbozzi <ANDREA.BARBOZZI@local.ittcesena.it>
2024-05-21 12:51:26 +0200

    Aula

--- Banchi/AulaWindow.xaml.cs
+++ Banchi/AulaWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Label = System.Windows.Controls.Label;
 
 namespace Banchi
 {
@@ -19,26 +21,52 @@ namespace Banchi
     /// </summary>
     public partial class AulaWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        Label grafica = new Label();
+        Aula a1;
+        bool isDragging = false;
+        private Point startPosition;
+
         public AulaWindow()
         {
             InitializeComponent();
+            a1 = new Aula("L12", 200, 100);
+            canvasC.Children.Add(grafica);
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(1);
+            timer.Start();
+            timer.Tick += Timer_Tick;
+            // canvasC.Children.Add(grafica); spostato sopra, non dovrebbe cambiare nulla
+
             FinestrePorte.Visibility = Visibility.Hidden;
-            Canvas.Visibility = Visibility.Hidden;
-            
+            canvasC.Visibility = Visibility.Visible;
         }
 
-        private void btnClick_ConfermaDim(object sender, RoutedEventArgs e)
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            double altezza2 = Convert.ToDouble(txtAltezza.Text);
+            double base2 = Convert.ToDouble(txtBase.Text);
+            a1.AltezzaInMetri = altezza2;
+            a1.BaseInMetri = base2;
+            a1.NomeAula = txtNome.Text;
+        }
+
+        private void btnClick_Conferma(object sender, RoutedEventArgs e)
         {
             if (Convert.ToDouble(txtBase.Text) > 0.0 && Convert.ToDouble(txtAltezza.Text) > 0.0)
             {
                 DimensioniAula.Visibility = Visibility.Hidden;
                 FinestrePorte.Visibility = Visibility.Visible;
-                Canvas.Visibility = Visibility.Visible;
+                canvasC.Visibility = Visibility.Visible;
                 double base2 = Convert.ToDouble(txtBase.Text);
                 double altezza2 = Convert.ToDouble(txtAltezza.Text);
-                Aula aula1 = new(txtNome, altezza2, base2);
-                
+                Aula aula1 = new(txtNome.Text, altezza2, base2);
             }
+        }
+
+        private void btn_ConfermaFinestra_Click(object sender, RoutedEventArgs e)
+        {
 
         }
     }
