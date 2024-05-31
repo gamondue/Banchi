73665e5f0d54f64866798889a2348187ff0f2948
Nicolò Casavecchia <162800508+MrSasso@users.noreply.github.com>
2024-05-31 14:30:53 +0200

    cambiamenti su computer e su ordinamento lista (#17)
    * aggiunta di ordinamenti per la lista degli studenti
    
    * ho messo i dati all'interno dei corrispondenti textblock, quindi se si seleziona un computer dalla lista sarà possibile vederne i relativi dati, altrimenti, non selezionandone nessuno, è possibile crearne uno nuovo
    
    * aggiustato ordinamento lista studenti e aggiunto attribuzione studenti a banchi
    
    ---------
    
    Co-authored-by: Nicolo Casavecchia <NICOLO.CASAVECCHIA@local.ittcesena.it>
    Co-authored-by: Riccardo Bellavista <riccardo.bellavista@local.ittcesena.it>

--- Banchi/BusinessLayer/BusinessLayer.cs
+++ Banchi/BusinessLayer/BusinessLayer.cs
@@ -106,5 +106,95 @@ namespace Banchi
         {
             return DataLayer.LeggiBanchiDellAula(aula);
         }
+        internal static List<Studente> ordinamentoCasualeListaStudenti(List<Studente> listaStudenti)
+        {
+            List<Studente> listaStudentiRandom = new List<Studente>(0);
+            if (listaStudenti != null)
+            {
+                bool[] antiRipetizioneRandom = new bool[listaStudenti.Count()];
+                for (int i = listaStudenti.Count() - 1; i > 0; i--)
+                {
+                    Random studenteRandom = new Random();
+                    int numero = studenteRandom.Next(i + 1);
+                    int posizioneCicloRandom = 0;
+                    for (int j = 0; j < listaStudenti.Count(); j++)
+                    {
+                        if (antiRipetizioneRandom[j] != true)
+                        {
+                            if (posizioneCicloRandom.ToString() == numero.ToString())
+                            {
+                                listaStudentiRandom.Add(listaStudenti[j]);
+                                antiRipetizioneRandom[j] = true;
+                                break;
+                            }
+                            else
+                                posizioneCicloRandom++;
+                        }
+                    }
+                }
+                for (int i = 0; i < listaStudenti.Count(); i++)
+                {
+                    if (antiRipetizioneRandom[i] != true)
+                    {
+                        listaStudentiRandom.Add(listaStudenti[i]);
+                        break;
+                    }
+                }
+
+            }
+            return listaStudentiRandom;
+        }
+        internal static List<Studente> ordinamentoAlfabeticoListaStudenti(List<Studente> listaStudenti)
+        {
+            if (listaStudenti != null)
+            {
+                for (int i = 0; i < listaStudenti.Count() - 1; i++)
+                {
+                    string cognomeSort = listaStudenti[i].Cognome;
+                    int indiceCognomeSort = i;
+
+                    for (int j = i; j < listaStudenti.Count(); j++)
+                    {
+                        //se è 1 scambio e se è -1 no scambio
+                        int valoreDiScambioDelSort = String.Compare(cognomeSort, listaStudenti[j].Cognome);
+                        if (valoreDiScambioDelSort > 0)
+                        {
+                            cognomeSort = listaStudenti[j].Cognome;
+                            indiceCognomeSort = j;
+                        }
+                    }
+
+                    Studente supporto = listaStudenti[i];
+                    listaStudenti[i] = listaStudenti[indiceCognomeSort];
+                    listaStudenti[indiceCognomeSort] = supporto;
+                }
+            }
+            return listaStudenti;
+        }
+        internal static List<Studente> ordinamentoVotoListaStudenti(List<Studente> listaStudenti)
+        {
+            if (listaStudenti != null)
+            {
+                for (int i = 0; i < listaStudenti.Count() - 1; i++)
+                {
+                    double votoSort = listaStudenti[i].Voto;
+                    int indiceVotoSort = i;
+
+                    for (int j = i; j < listaStudenti.Count(); j++)
+                    {
+                        if (listaStudenti[j].Voto > votoSort)
+                        {
+                            votoSort = listaStudenti[j].Voto;
+                            indiceVotoSort = j;
+                        }
+                    }
+
+                    Studente supporto = listaStudenti[i];
+                    listaStudenti[i] = listaStudenti[indiceVotoSort];
+                    listaStudenti[indiceVotoSort] = supporto;
+                }
+            }
+            return listaStudenti;
+        }
     }
 }
