6622ef0f676c9fb29df0f009289c9533898f9f59
gamon su Goku <ing@ingmonti.it>
2024-06-18 17:02:54 +0200

    Sync commit, does NOT compile!
    ComputerWindow: refactorings per renderal adattabile alla . variazione delle dimensioni della finestra. Aggiunta una parte in basso che mostra i computer che esistone ed una lista di computer. Aggiuntea la parte di UI per creare le lista.
    Computer (classe): aggiunto lo stato (anche nella finestra)
    BusinessLayer: aggiunto metodo GeneraComputer, che genera tutti i comouter di un laboratorio (DA PROVARE)
    MainWindow: nella distribuzione automatica degli studenti, si evita di mettere studenti nelle cattedre,

--- Banchi/ComputerWindow.xaml.cs
+++ Banchi/ComputerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.DirectoryServices.ActiveDirectory;
+using System.Windows;
 
 namespace Banchi
 {
@@ -18,42 +19,69 @@ namespace Banchi
         Computer computer;
         MessageBoxButton bottone = MessageBoxButton.YesNo;
         MessageBoxResult result;
+        private List<Computer>? tuttiIComputer;
+        private Computer currentComputer;
+
         public ComputerWindow(Aula aula, Computer computer = null)
         {
             InitializeComponent();
-            
+
+            currentComputer = computer;
+             
             if (computer != null)
             {
-                NomeDispositivoMod.Text = computer.ToString();
-                MarcaMod.Text = computer.MarcaComputer;
-                ProcessoreMod.Text = computer.Processore;
-                TipoSistemaMod.Text = computer.TipoSistema;
-                IPMod.Text = computer.IndirizzoIPComputer;
+                IPMod.IsReadOnly = false;
+                NomeDispositivoMod.IsReadOnly = false;
+                MarcaMod.IsReadOnly = false;
+                NoteComputer.IsReadOnly = false;
+                ProcessoreMod.IsReadOnly = false;
+                TipoSistemaMod.IsReadOnly = false;
+
+                FromObjectToUi(computer);
             }
-               
-            
-            
+            tuttiIComputer = BusinessLayer.LeggiTuttiIComputer();
+            ComputerGrid.ItemsSource = BusinessLayer.LeggiTuttiIComputer();
 
             this.computer = computer;
         }
-
+        private void FromObjectToUi(Computer computer)
+        {
+            NomeDispositivoMod.Text = computer.NomeDispositivo;
+            MarcaMod.Text = computer.MarcaComputer;
+            ProcessoreMod.Text = computer.Processore;
+            TipoSistemaMod.Text = computer.TipoSistema;
+            IPMod.Text = computer.IndirizzoIPComputer;
+            NoteComputer.Text = computer.NoteComputer;
+            StatoComputer.SelectedText = computer.Stato;
+        }
+        private Computer FromUiToComputer()
+        {
+            Computer computer = new(NomeDispositivoMod.Text);
+            computer.MarcaComputer = MarcaMod.Text;
+            computer.Processore = ProcessoreMod.Text;
+            computer.TipoSistema = TipoSistemaMod.Text;
+            computer.IndirizzoIPComputer = IPMod.Text;
+            computer.NoteComputer = NoteComputer.Text;
+            computer.Stato = StatoComputer.SelectedText;
+            return computer;
+        }
         private void SegnalazioneWindow_click(object sender, RoutedEventArgs e)
         {
             SegnalazioneWindow wnd = new SegnalazioneWindow(computer);
             if (result == MessageBoxResult.Yes) 
             {
                 wnd.Show();
-            }
-           
+            }     
         }
         private void ModificaComputer_Click(object sender, RoutedEventArgs e)
         {
             IPMod.IsReadOnly = false;
             NomeDispositivoMod.IsReadOnly = false;
             MarcaMod.IsReadOnly = false;
-            NotaComputer.IsReadOnly = false;
+            ProcessoreMod.IsReadOnly = false;
             ProcessoreMod.IsReadOnly = false;
             TipoSistemaMod.IsReadOnly = false;
+            StatoComputer.SelectedText = "In uso";
         }
         private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
@@ -66,6 +94,34 @@ namespace Banchi
         private void SalvaButton_Click(object sender, RoutedEventArgs e)
         {
             BusinessLayer.SalvaComputer();
+        }
+        private void GestisciLab_Click(object sender, RoutedEventArgs e)
+        {
+            this.Height = 600;
+
+        }
+        private void GeneraComputer_Click(object sender, RoutedEventArgs e)
+        {
+            Computer tempComputer = FromUiToComputer();
+
+            List<Computer> listaNuoviAula = BusinessLayer.GeneraComputer(txtSchemaNome.Text, tempComputer,
+                txtNumeroComputerInizio.Text, txtNumeroComputerFine.Text);
+            lstComputerLab.ItemsSource = listaNuoviAula;
+        }
+        private void ComputerGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            currentComputer = ComputerGrid.SelectedItem;
+            FromObjectToUi(currentComputer);
+            //DataContext = null;
+            //DataContext = currentComputer;
+        }
+        private void RimuoviComputer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void AggiungiComputer_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
